c8276ab5b792cc8b9c67c77fb5ba6639863ba2f7
junto <juntosmz36@gmail.com>
2026-06-26 13:33:46 +0900

    穴埋め前

---
+++ week10_1.py
@@ -0,0 +1,37 @@
+# 穴埋め問題用のプログラム
+from def_graph import Graph  # Graphクラスの読み込み
+
+class GraphWalk(Graph):
+    def walk(self, start, step):
+        visited = [start]  # 開始地点のノードIDを訪問済みリストに追加
+        parents = {start:None}  # 直前のノードとそれに接続するエッジのペア（タプル）を格納する辞書（key=訪問中ノードID、value=(直前のノードID, エッジラベル)）
+        current = start  # 開始地点のノードIDを探索基点のノードに設定
+        for i in range(step):  # step分、訪問を繰り返す
+            if current not in self.node_list:  # 訪問中ノードが存在しなければ
+                print('{}のノードがグラフ上に存在しません。'.format(current))
+                return
+            neighbors = self.get_neighborhood([____])  # 訪問中ノードの隣接ノードを取得
+            if neighbors != []:  # 隣接ノードがあれば以下を実行
+                visited.append(neighbors[0].id)  # 次の訪問先を訪問する
+                parents[[____]] = ([____], neighbors[0].label)  # 訪問中ノードIDとそれに接続するエッジをparents辞書に追加
+                current = [____]  # 訪問先候補を次の訪問先に決定
+            else:  # 隣接ノードが無ければ終了
+                print('{}から{}ステップ分進むパスはありません。'.format(start, step))
+                return
+        # 訪問したノードを訪問順に表示する
+        path = visited[0]  # 最初に訪問したノードをpathに代入
+        for v in visited[1:]:  # 訪問した順にノードを取得
+            path = path + ' --[' + parents[v][1] +']--> ' + v  # 次の訪問先と辿ったエッジをpathに連結
+        print(path)
+
+
+if __name__ == '__main__':  # モジュールとしてインポートされるときは実行しない
+    fi = open('kyotocitybus_line.dat', 'r', encoding = 'utf-8')
+    bus_network = GraphWalk()
+    lines = fi.readlines()
+    for line in lines:
+        line = line.rstrip()
+        items = line.split(' ')  # 1行を半角スペースで区切ってitemsリストに代入
+        bus_network.add_undirected_edge(items[0], items[1], items[3])  # 無向エッジを追加
+    bus_network.walk('ED01_1914', 5)
+    fi.close()
